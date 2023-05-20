7acc57969b540c3ef5ba153e523533f13e28d699
prolajumokeoni <prolajumokeoni@gmail.com>
2023-05-20 23:21:47 +0100

    Update cart total

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -38,12 +38,14 @@ export const CartContext = createContext({
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     const newCartCount = cartItems.reduce(
@@ -52,6 +54,15 @@ export const CartProvider = ({ children }) => {
     );
 
     setCartCount(newCartCount);
+  }, [cartItems]);
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce (
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+
+    setCartTotal(newCartTotal);
   }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
@@ -73,6 +84,7 @@ export const CartProvider = ({ children }) => {
     cartCount,
     clearItemFromCart,
     removeItemFromCart,
+    cartTotal,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
